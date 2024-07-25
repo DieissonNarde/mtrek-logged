39bd30af58d810a74d92a0638c3bdce2ee13052a
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2024-07-25 14:36:43 -0300

    icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-light': '#D9D9D9',
+      },
+    },
   },
   plugins: [],
 };
