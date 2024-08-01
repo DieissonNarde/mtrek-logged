de5a9fb0919ea9c5e6588a7de825e24a0ad5174b
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2024-08-01 10:34:38 -0300

    att ( body nao scrolando quando abre sidebar e quando clica no item dentro do sidebar ele fecha )

--- src/scripts/info-recibo.js
+++ src/scripts/info-recibo.js
@@ -1,20 +1,39 @@
 let isReciboProcessing = false; // Variável para evitar múltiplas execuções no reciboForm
 
-// Função para habilitar ou desabilitar o botão de envio no reciboForm
+// Função para habilitar ou desabilitar o botão de envio no reciboForm e estilizar os campos
 function toggleReciboSubmitButton() {
     const form = document.getElementById('reciboForm');
     const submitButton = document.getElementById('enviarReciboBtn');
     const isValid = form.checkValidity();
+
+    // Estiliza o botão de envio
     submitButton.disabled = !isValid;
     submitButton.style.background = isValid ? '#5F7336' : '#D9D9D9';
     submitButton.style.color = isValid ? '#ffff' : '#000';
+
+    // Estiliza os campos de entrada e selects
+    const fields = ['data_envio', 'recibo-distribuidora'];
+    fields.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.style.borderColor = isValid ? '#5F7336' : '#D9D9D9';
+            element.style.color = isValid ? '#000' : '#AEAEAE';
+        }
+    });
 }
 
-// Função para atualizar o nome do arquivo carregado no reciboForm
+// Função para atualizar o nome do arquivo carregado no reciboForm e estilizar o botão de upload
 function updateReciboFileName(input) {
     const fileNamePlaceholder = document.getElementById('recibo-file-name-placeholder');
+    const fileBtn = document.getElementById('recibo-file-btn');
     const file = input.files[0];
+    
+    // Atualiza o texto do placeholder com o nome do arquivo ou mensagem padrão
     fileNamePlaceholder.textContent = file ? file.name : 'Tipos permitidos: .jpg, .png, .pdf, .doc';
+    
+    // Estiliza o botão de upload com base na presença de um arquivo
+    fileBtn.style.backgroundColor = file ? '#5F7336' : '#D9D9D9';
+    fileBtn.style.color = file ? '#fff' : '#000';
 }
 
 // Função para lidar com o envio do formulário no reciboForm
@@ -48,6 +67,11 @@ function submitReciboForm(event) {
         if (fileBtnText) {
             fileBtnText.textContent = 'Carregar outro';
         }
+
+        // Atualiza os estilos após o envio
+        const fileBtn = document.getElementById('recibo-file-btn');
+        fileBtn.style.backgroundColor = ''; // Cor roxa
+        fileBtn.style.color = '#fff'; // Texto branco
 
         toggleReciboSubmitButton();
         isReciboProcessing = false;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,81 +1,81 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menuLinks = document.querySelectorAll('.menu__list__item__link');
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const menuLinks = document.querySelectorAll('.menu__list__item__link');
-
-    function showSection(targetId) {
-      document.querySelectorAll('.route').forEach(function(section) {
-        section.classList.add('hidden');
-      });
-      const targetSection = document.getElementById(targetId);
-      if (targetSection) {
-        targetSection.classList.remove('hidden');
-      }
+  function showSection(targetId) {
+    document.querySelectorAll('.route').forEach(function(section) {
+      section.classList.add('hidden');
+    });
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.classList.remove('hidden');
     }
+  }
 
-    // Lida com hash na URL
-    const hash = window.location.hash.substring(1);
-    if (hash) {
-      showSection(hash);
-    }
+  // Lida com hash na URL
+  const hash = window.location.hash.substring(1);
+  if (hash) {
+    showSection(hash);
+  }
 
-    menuLinks.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = link.getAttribute('data-target');
-        showSection(targetId);
-        // Atualiza a URL com hash
-        window.location.hash = targetId;
-      });
+  menuLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const targetId = link.getAttribute('data-target');
+      showSection(targetId);
+      // Atualiza a URL com hash
+      window.location.hash = targetId;
+      // Fecha o sidebar e remove a classe no-scroll
+      closeSidebar();
     });
   });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+  const menuResponsiveImg = document.querySelectorAll('.menu_responsivo');
+  const arrowHeaderResponsive = document.querySelector('.arrow_header_responsive');
+  const header = document.getElementById('header');
+  const sidebar = document.getElementById('sidebar');
+  const overlay = document.getElementById('overlay');
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona a imagem do menu responsivo e o botão de seta responsiva
-    const menuResponsiveImg = document.querySelectorAll('.menu_responsivo');
-    const arrowHeaderResponsive = document.querySelector('.arrow_header_responsive');
-    
-    // Seleciona o header, o aside e a sobreposição
-    const header = document.getElementById('header');
-    const sidebar = document.getElementById('sidebar');
-    const overlay = document.getElementById('overlay');
-    
-    // Função para alternar a visibilidade do header, sidebar e overlay
-    function toggleVisibility() {
-      header.classList.toggle('hidden');
-      sidebar.classList.toggle('hidden');
-      overlay.classList.toggle('active');
-    }
-  
-    // Adiciona eventos de clique para a imagem do menu responsivo
-    if (menuResponsiveImg) {
-      menuResponsiveImg.forEach(function(img) {
-        img.addEventListener('click', toggleVisibility)
-      });
-    }
-  
-    // Adiciona eventos de clique para o botão de seta responsiva
+  function toggleVisibility() {
+    header.classList.toggle('hidden');
+    sidebar.classList.toggle('hidden');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('no-scroll'); // Adiciona ou remove a classe no-scroll
+  }
+
+  function closeSidebar() {
+    header.classList.add('hidden');
+    sidebar.classList.add('hidden');
+    overlay.classList.remove('active');
+    document.body.classList.remove('no-scroll'); // Remove a classe no-scroll
     if (arrowHeaderResponsive) {
-      arrowHeaderResponsive.addEventListener('click', function () {
-        toggleVisibility();
-        arrowHeaderResponsive.classList.toggle('active');
-      });
+      arrowHeaderResponsive.classList.remove('active');
     }
-  
-    // Adiciona um evento de clique para a sobreposição para fechar o menu quando clicado
-    if (overlay) {
-      overlay.addEventListener('click', function () {
-        header.classList.add('hidden');
-        sidebar.classList.add('hidden');
-        overlay.classList.remove('active');
-        if (arrowHeaderResponsive) {
-          arrowHeaderResponsive.classList.remove('active');
-        }
-      });
-    }
-  });
+  }
 
-  // Lógica do modal
+  if (menuResponsiveImg) {
+    menuResponsiveImg.forEach(function(img) {
+      img.addEventListener('click', toggleVisibility);
+    });
+  }
+
+  if (arrowHeaderResponsive) {
+    arrowHeaderResponsive.addEventListener('click', function () {
+      toggleVisibility();
+      arrowHeaderResponsive.classList.toggle('active');
+    });
+  }
+
+  if (overlay) {
+    overlay.addEventListener('click', closeSidebar);
+  }
+
+  // Fecha o sidebar ao clicar em qualquer item dentro dele
+  sidebar.querySelectorAll('*').forEach(function(item) {
+    item.addEventListener('click', closeSidebar);
+  });
+});
 
 function openModal() {
   document.getElementById("idModal").style.display = "flex";
@@ -96,5 +96,3 @@ function submitDocuments() {
   closeModal();
   alert("Documentos enviados com sucesso!");
 }
-
-  
