194f19ee2114bb25dfdcb3bc22588b3c1789ca35
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2024-08-05 21:41:05 -0300

    att

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,112 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const menuLinks = document.querySelectorAll('.menu__list__item__link');
-
-//   function showSection(targetId) {
-//     document.querySelectorAll('.route').forEach(function(section) {
-//       section.classList.add('hidden');
-//     });
-//     const targetSection = document.getElementById(targetId);
-//     if (targetSection) {
-//       targetSection.classList.remove('hidden');
-//     }
-//   }
-
-//   // Lida com hash na URL
-//   const hash = window.location.hash.substring(1);
-//   if (hash) {
-//     showSection(hash);
-//     setActiveLink(hash); // Adiciona a classe .active ao link correspondente ao hash
-//   }
-
-//   function setActiveLink(targetId) {
-//     menuLinks.forEach(function(link) {
-//       link.classList.remove('active');
-//       if (link.getAttribute('data-target') === targetId) {
-//         link.classList.add('active');
-//       }
-//     });
-//   }
-
-//   menuLinks.forEach(function(link) {
-//     link.addEventListener('click', function(event) {
-//       event.preventDefault();
-//       const targetId = link.getAttribute('data-target');
-//       showSection(targetId);
-//       setActiveLink(targetId); // Adiciona a classe .active ao link clicado
-//       // Atualiza a URL com hash
-//       window.location.hash = targetId;
-//       // Fecha o sidebar e remove a classe no-scroll
-//       closeSidebar();
-//     });
-//   });
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const menuResponsiveImg = document.querySelectorAll('.menu_responsivo');
-//   const arrowHeaderResponsive = document.querySelector('.arrow_header_responsive');
-//   const header = document.getElementById('header');
-//   const sidebar = document.getElementById('sidebar');
-//   const overlay = document.getElementById('overlay');
-
-//   function toggleVisibility() {
-//     header.classList.toggle('hidden');
-//     sidebar.classList.toggle('hidden');
-//     overlay.classList.toggle('active');
-//     document.body.classList.toggle('no-scroll'); // Adiciona ou remove a classe no-scroll
-//   }
-
-//   function closeSidebar() {
-//     header.classList.add('hidden');
-//     sidebar.classList.add('hidden');
-//     overlay.classList.remove('active');
-//     document.body.classList.remove('no-scroll'); // Remove a classe no-scroll
-//     if (arrowHeaderResponsive) {
-//       arrowHeaderResponsive.classList.remove('active');
-//     }
-//   }
-
-//   if (menuResponsiveImg) {
-//     menuResponsiveImg.forEach(function(img) {
-//       img.addEventListener('click', toggleVisibility);
-//     });
-//   }
-
-//   if (arrowHeaderResponsive) {
-//     arrowHeaderResponsive.addEventListener('click', function () {
-//       toggleVisibility();
-//       arrowHeaderResponsive.classList.toggle('active');
-//     });
-//   }
-
-//   if (overlay) {
-//     overlay.addEventListener('click', closeSidebar);
-//   }
-
-//   // Fecha o sidebar ao clicar em qualquer item dentro dele
-//   sidebar.querySelectorAll('*').forEach(function(item) {
-//     item.addEventListener('click', closeSidebar);
-//   });
-// });
-
-// function openModal() {
-//   document.getElementById("idModal").style.display = "flex";
-// }
-
-// function closeModal() {
-//   document.getElementById("idModal").style.display = "none";
-// }
-
-// window.onclick = function(event) {
-//   if (event.target == document.getElementById("idModal")) {
-//     closeModal();
-//   }
-// }
-
-// function submitDocuments() {
-//   // Aqui você pode adicionar a lógica para submissão dos documentos
-//   closeModal();
-//   alert("Documentos enviados com sucesso!");
-// }
 document.addEventListener('DOMContentLoaded', function () {
   const menuLinks = document.querySelectorAll('.menu__list__item__link');
   const menuResponsiveImg = document.querySelectorAll('.menu_responsivo');
@@ -237,3 +128,30 @@ function submitDocuments() {
   closeModal();
   alert("Documentos enviados com sucesso!");
 }
+
+
+// Seleciona todos os elementos <select> com a classe 'select-toggle'
+const selectElements = document.querySelectorAll('.select-toggle');
+let openSelect = null;
+
+selectElements.forEach(select => {
+  // Adiciona evento de clique para alternar a rotação
+  select.addEventListener('click', function(event) {
+    // Evita o comportamento padrão de rotação do select ao clicar dentro dele
+    event.stopPropagation();
+    
+    if (openSelect && openSelect !== this) {
+      openSelect.classList.remove('rotated');
+    }
+    this.classList.toggle('rotated');
+    openSelect = this.classList.contains('rotated') ? this : null;
+  });
+});
+
+// Adiciona evento para clique fora dos <select>
+document.addEventListener('mousedown', function(event) {
+  if (openSelect && !Array.from(selectElements).includes(event.target)) {
+    openSelect.classList.remove('rotated');
+    openSelect = null;
+  }
+});
